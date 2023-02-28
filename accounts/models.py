b3e3428ed40fb253ea39e8984eab7311a660ee78
Roshan4004 <gautamroshan4004@gmail.com>
2023-02-28 22:44:59 +0545

    OTP verification, designs

--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,12 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    auth_tokens=models.CharField(max_length=150 )
     is_verified=models.BooleanField(default=False)
     created_at=models.DateField(auto_now_add=True)
+    country=models.TextField(blank=False)
+    local_address=models.TextField(blank=True)
+    bio=models.TextField(blank=True)
+    profile_img=models.ImageField(default='userimage/user-icon.png',upload_to='uploads/userimage/')
 
     def __str__(self):
         return self.user.username
