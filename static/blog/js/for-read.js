8aa685555f923d166d0d9d9df85d389c828685b8
Roshan4004 <gautamroshan4004@gmail.com>
2024-12-03 20:57:59 +0545

    Read Button and functionality done
    Only English language is available for now as I am working with Nepali translation currently...

---
+++ static/blog/js/for-read.js
@@ -0,0 +1,39 @@
+
+const synth = window.speechSynthesis;
+
+function read(){
+      $(".read-box").css("display","flex");
+      $("#play-button").attr("src","/static/blog/pause.png")
+
+}
+
+
+$("#play-button").on('click',function(e){
+   e.preventDefault();
+
+   if(synth.paused){
+      synth.resume();
+      $("#play-button").attr("src","/static/blog/pause.png")
+   }else if(synth.speaking){
+      synth.pause();
+      $("#play-button").attr("src","/static/blog/play.png")
+   }else{
+      get_contents();
+      $("#play-button").attr("src","/static/blog/pause.png")      
+
+   }
+
+})
+
+function get_contents(){
+   var j=$('div#content_div p');
+   let split=$('div#content_div').html().split('\n\n')
+   split.forEach(function(item){speak(item.replace(/<[^>]*>/g, ''))})
+}
+
+function speak(ok) {
+   utterance = new SpeechSynthesisUtterance(ok);
+   voices = synth.getVoices();
+   utterance.voice = voices[4];
+   synth.speak(utterance);
+ }
